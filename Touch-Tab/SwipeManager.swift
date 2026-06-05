3da40e5c8d71572da1711f440fd7b2e7fcdb680e
Shell Ou <shell@Shells-MacBook-Pro.local>
2026-06-04 21:43:54 -0700

    Increase 3-finger swipe sensitivity and reduce initial rate-limiting delay

--- Touch-Tab/SwipeManager.swift
+++ Touch-Tab/SwipeManager.swift
@@ -1,9 +1,9 @@
 import Cocoa
 
 class SwipeManager {
-    private static let accVelXThreshold: Float = 0.07
+    private static let accVelXThreshold: Float = 0.012
     // TODO: figure out the real value of the delay.
-    private static let appSwitcherUIDelay: Double = 0.2
+    private static let appSwitcherUIDelay: Double = 0.05
 
     private static var eventTap: CFMachPort? = nil
     // Event state.
