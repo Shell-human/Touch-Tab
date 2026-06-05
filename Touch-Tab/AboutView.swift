3ecd9db5de115115a2c2fca302077d291263cd6c
Shell Ou <shell@Shells-MacBook-Pro.local>
2026-06-05 09:47:17 -0700

    Fix: Optimize AboutView height to eliminate excess bottom whitespace

--- Touch-Tab/AboutView.swift
+++ Touch-Tab/AboutView.swift
@@ -174,7 +174,7 @@ struct AboutView: View {
             }
             .padding(20)
         }
-        .frame(width: 320, height: appState.isTrusted ? 510 : 610)
+        .frame(width: 320, height: appState.isTrusted ? 450 : 540)
     }
 }
 
